a8f1e50aea4d34298427b05bfb2b38065f806dec
BambooCode98 <jw.thomas98@yahoo.com>
2022-03-24 23:30:20 -0500

    finished with sections 1-3

--- src/components/second.js
+++ src/components/second.js
@@ -9,13 +9,16 @@ export default function SecondSection() {
 
   return (
      <div className="body2">
-      <h1 className="header2">Current Knowledge</h1>
-      <ul className="sec2para">
+      <h1 className="header2">Current Programming Knowledge</h1>
+      <ul className="sec2list">
         <li className="html5">HTML 5</li>
         <li className="css3">CSS 3</li>
         <li className="js">JS</li>
         <li className="react">React</li>
+        <li className="git">Git</li>
+        <li className="npm">npm</li>
       </ul>
+      <p className="sec2text">The listed programming languages/frameworks reflect my ability to be able to create a project from scratch with the knowledge I currently have.</p>
      </div>
   );
 }
--- src/components/third.js
+++ src/components/third.js
@@ -7,6 +7,24 @@ export default function ThirdSection() {
   return(
     <div className="body3">
       <h1 className="header3">Projects I Have Created</h1>
+      {/* <div className='proContainer'> */}
+        <img 
+        src={ require('../images/projects/dcvillains.png')}
+        className='dc'
+        />
+        <img 
+        src={ require('../images/projects/storepage.png')}
+        className='storepage'
+        />
+        <img 
+        src={ require('../images/projects/todolist.png')}
+        className='todolist'
+        />
+        <img 
+        src={ require('../images/projects/weatherapp.png')}
+        className='weatherapp'
+        />
+      {/* </div> */}
     </div>
   );
 }
