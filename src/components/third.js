0f41a5a84e7e722cea57622628bf01472155c48d
BambooCode98 <jw.thomas98@yahoo.com>
2022-03-25 13:19:21 -0500

    implemented a project slideshow

--- src/components/third.js
+++ src/components/third.js
@@ -1,32 +1,75 @@
+import { useState, useEffect } from 'react';
+import dc from '../images/projects/dcvillains.png';
+import storepage from '../images/projects/storepage.png';
+import todolist from '../images/projects/todolist.png';
+import weatherapp from '../images/projects/weatherapp.png';
 
-
+const images = [
+  {
+    path: dc, 
+    title: 'dc', 
+    id: 1
+  },
+  {
+    path: storepage, 
+    title: 'storepage', 
+    id: 2
+  }, 
+  {
+    path: todolist, 
+    title: 'todolist',
+    id: 3
+  }, 
+  {
+    path: weatherapp, 
+    title: 'weatherapp',
+    id: 4
+  }
+]
 
 export default function ThirdSection() {
-
+  const [index, setIndex] = useState(0);
+  useEffect(() => {
+    
+  }, [index])
   
+
+  function leftClick() {
+    setIndex( value => {
+      if(value > 0) {
+        return value - 1;
+      } else if(value === 0) {
+        return value;
+      }
+    })
+  }
+
+  function rightClick() {
+    setIndex(value => {
+      if(value < 3) {
+        return value + 1
+      } else if(value === 3) {
+        return value;
+      }
+    })
+  }
 
   return(
     <div className="body3">
       <h1 className="header3">Projects I Have Created</h1>
-      {/* <div className='proContainer'> */}
+      <div className='projContainer'>
+        <p className='leftArrow' onClick={leftClick}>&lt;</p>
         <img 
-        src={ require('../images/projects/dcvillains.png')}
-        className='dc'
-        // onMouseEnter={}
+        src={images[index].path}
+        alt={images[index].title}
+        className={images[index].title}
         />
-        <img 
-        src={ require('../images/projects/storepage.png')}
-        className='storepage'
-        />
-        <img 
-        src={ require('../images/projects/todolist.png')}
-        className='todolist'
-        />
-        <img 
-        src={ require('../images/projects/weatherapp.png')}
-        className='weatherapp'
-        />
-      {/* </div> */}
+        <p className='rightArrow' onClick={rightClick}>&gt;</p>
+        <div className='dot1' />
+        <div className='dot2' />
+        <div className='dot3' />
+        <div className='dot4' />
+      </div>
     </div>
   );
 }
